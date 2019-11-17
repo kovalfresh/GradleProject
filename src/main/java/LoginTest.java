30f1f958ca8e3620f2ae365f3163f024eca558d7
koval <kovalfresh@gmail.com>
2019-11-17 18:10:05 +0200

    h

--- src/main/java/LoginTest.java
+++ src/main/java/LoginTest.java
@@ -1,22 +1,40 @@
- import org.openqa.selenium.*;
+import com.codeborne.selenide.Condition;
+import org.openqa.selenium.*;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Properties;
+
 import static com.codeborne.selenide.Selenide.*;
+import static org.testng.Assert.assertTrue;
 
 public class LoginTest {
 
-    String basicURL = "http://forge.f-d.com.ua";
-    String login = "info@fresh-design.com.ua";
-    String password = "info@fresh-design.com.ua";
+    //String basicURL = "http://forge.f-d.com.ua";
+    String basicURL;
+    String login;
+    String password;
+
+    Properties prop = new Properties();
 
     @Test
     void loginTest() throws InterruptedException {
 
+        try {
+            basicURL = RunConfig.readFromConfig("basicURL");
+            login = RunConfig.readFromConfig("login");
+            password = RunConfig.readFromConfig("password");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new InterruptedException();
+        }
+
         open(basicURL);
-        $(By.name("loginButton")).isDisplayed();
+        $(By.name("loginButton")).shouldBe(Condition.visible);
         $(By.cssSelector("#adminloginform-email")).setValue(login);
         $(By.cssSelector("#adminloginform-password")).setValue(password);
         $(By.name("loginButton")).click();
-        $(By.className("user")).isDisplayed();
+        $(By.className("user")).shouldBe(Condition.visible);
 
         Thread.sleep(5000);
     }
---
+++ src/main/java/RunConfig.java
@@ -0,0 +1,18 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class RunConfig {
+
+    public static String readFromConfig(String stringProp) throws IOException {
+
+        Properties prop = new Properties();
+        InputStream input = new FileInputStream("src/main/resources/system.properties");
+        prop.load(input);
+        return prop.getProperty(stringProp);
+
+    }
+
+}
